4848bbddf260e4f64e242df4aea54915716b3b6b
Rendani Sinyage <54802466+RendaniSinyage@users.noreply.github.com>
2026-04-07 14:21:51 +0200

    Stabilize Flutter build: Module segregation and model unification (#152)

--- lib/infrastructure/models/data/referral_data.dart
+++ lib/infrastructure/models/data/referral_data.dart
@@ -1,4 +1,4 @@
-import 'package:rokctapp/infrastructure/models/data/manager/translation.dart';
+import 'package:rokctapp/infrastructure/models/data/translation.dart';
 
 class ReferralModel {
   String? id;
