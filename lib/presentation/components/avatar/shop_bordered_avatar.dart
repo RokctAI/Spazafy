dff906d70cd7513af18876cc3448da18f2489e0e
Rendani Sinyage <sinyage@gmail.com>
2026-04-12 11:50:33 +0200

    Update shop_bordered_avatar.dart

--- lib/presentation/components/avatar/shop_bordered_avatar.dart
+++ lib/presentation/components/avatar/shop_bordered_avatar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:rokctapp/presentation/components/helper/blur_wrap.dart';
-import 'common_image.dart';
+import 'package:rokctapp/presentation/components/image/common_image.dart';
 import 'package:rokctapp/presentation/theme/app_style.dart';
 
 class ShopBorderedAvatar extends StatelessWidget {
