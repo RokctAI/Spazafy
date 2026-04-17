5987fb1eac490869cf9866fdfae02d8e587f5832
Rendani Sinyage <sinyage@gmail.com>
2026-04-17 12:07:18 +0200

    Update extras_groups_response.dart

--- lib/infrastructure/models/response/extras_groups_response.dart
+++ lib/infrastructure/models/response/extras_groups_response.dart
@@ -1,6 +1,6 @@
 import 'package:rokctapp/infrastructure/models/data/extras.dart';
-import 'package:rokctapp/infrastructure/models/data/product_data.dart';
-import 'package:rokctapp/infrastructure/models/data/group.dart' hide Group;
+import 'package:rokctapp/infrastructure/models/data/product_data.dart' hide Group;
+import 'package:rokctapp/infrastructure/models/data/group.dart';
 
 class ExtrasGroupsResponse {
   ExtrasGroupsResponse({List<Group>? data}) {
