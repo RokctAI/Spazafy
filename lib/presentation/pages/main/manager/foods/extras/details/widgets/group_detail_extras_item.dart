e611ddfa032beb9041d3762ed3b31ead2e8bed4f
Rendani Sinyage <sinyage@gmail.com>
2026-04-17 12:20:03 +0200

    Update group_detail_extras_item.dart

--- lib/presentation/pages/main/manager/foods/extras/details/widgets/group_detail_extras_item.dart
+++ lib/presentation/pages/main/manager/foods/extras/details/widgets/group_detail_extras_item.dart
@@ -1,5 +1,6 @@
 import 'package:rokctapp/infrastructure/services/utils/local_storage.dart';
-import 'package:rokctapp/infrastructure/models/data/product_data.dart';
+import 'package:rokctapp/infrastructure/models/data/product_data.dart' hide Extras;
+
 import 'package:flutter/material.dart';
 import 'package:flutter_remix/flutter_remix.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
